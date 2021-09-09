cfbbbbe6b9cf33e4ea8488def0a594f37ad4be48
James Skelton <jamesskelton@Jamess-MacBook-Pro.local>
2021-09-09 17:05:05 -0400

    modifications to py script

--- model_iris.py
+++ model_iris.py
@@ -3,6 +3,8 @@ from sklearn.model_selection import cross_val_score
 from sklearn.tree import DecisionTreeClassifier
 import pickle
 import pandas as pd
+import argparser
+import sys
 
 def make_prediction(model, data):
     x = model.predict(data.drop('species', axis = 0)
@@ -13,7 +15,37 @@ def make_prediction(model, data):
 
 
 def main(model, data):
-    make_prediction(model, data)
+    parser = argparse.ArgumentParser(
+        description='''iris predictor. Run 'python %(prog)s <subcommand> --help' for subcommand help.''',
+        epilog=_examples,
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+
+    subparsers = parser.add_subparsers(help='Sub-commands', dest='command')
+
+    parser_gen = subparsers.add_parser('make_prediction', help='Generate images')
+    parser_gen.add_argument('--network', help='Network pickle filename', dest='network_pkl', required=True)
+    parser_gen.add_argument('--result-dir', help='Root directory for run results (default: %(default)s)', default='results', metavar='DIR')
+
+    args = parser.parse_args()
+    kwargs = vars(args)
+    subcmd = kwargs.pop('command')
+
+    if subcmd is None:
+        print ('Error: missing subcommand.  Re-run with --help for usage.')
+        sys.exit(1)
+
+    sc = dnnlib.SubmitConfig()
+    sc.num_gpus = 1
+    sc.submit_target = dnnlib.SubmitTarget.LOCAL
+    sc.local.do_not_copy_source_files = True
+    sc.run_dir_root = kwargs.pop('result_dir')
+    sc.run_desc = subcmd
+
+    func_name_map = {
+        'make_prediction': 'workflow-test.make_prediction'}
+    dnnlib.submit_run(sc, func_name_map[subcmd], **kwargs)
+
 
 if __name__ == "__main__":
 	main()
