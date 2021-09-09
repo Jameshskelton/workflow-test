d5ed5779c21e22b3fdf8c8faaa48eff7610feb8b
James Skelton <jamesskelton@Jamess-MacBook-Pro.local>
2021-09-09 16:37:37 -0400

    edited script

--- model_iris.py
+++ model_iris.py
@@ -5,10 +5,15 @@ import pickle
 import pandas as pd
 
 def make_prediction(model, data):
-    x = model.predict(data)
+    x = model.predict(data.drop('species', axis = 0)
     df = pd.DataFrame(iris.data)
     df['actual'] = iris.target
     df['pred'] = x
-    df.to_csv('iris_pred.csv')
+    df.to_csv('predSpecies.csv')
 
 
+def main(model, data):
+    make_prediction(model, data)
+
+if __name__ == "__main__":
+	main()
