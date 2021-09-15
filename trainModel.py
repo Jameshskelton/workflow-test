8361c8e968606792c4000afa6728fbbd47df5f14
James Skelton <jamesskelton@Jamess-MacBook-Pro.local>
2021-09-14 21:41:14 -0400

    m

--- trainModel.py
+++ trainModel.py
@@ -1,12 +1,13 @@
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.datasets import load_iris
+import pickle
 
 iris = load_iris()
 X = iris.data
 y = iris.target
 
-clf = tree.DecisionTreeClassifier()
+clf = DecisionTreeClassifier()
 clf = clf.fit(X, y)
 
-pickle.dump( favorite_color, open( "save.p", "wb" ) )
+pickle.dump( clf, open( "save.p", "wb" ) )
